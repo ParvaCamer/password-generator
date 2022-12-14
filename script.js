dc9ccb39a32474155457f0d8701849f5bf695b0c
ParvaCamer <64281030+ParvaCamer@users.noreply.github.com>
2022-12-14 23:49:46 +0100

    First function

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function rangeSlide(value) {
     document.getElementById('range-value').innerHTML = value;
 }
+
+function generatePassword() {
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    let valueInput = document.getElementById('length').value;
+    let array = [];
+    while (array.length < valueInput) {
+        let randomNumber = Math.floor(Math.random() * 26);
+        array.push(alphabet[randomNumber]);
+    }
+    console.log(array);
+}
